7dc1f1bd15b050d95d9a1f8b58a2850607f9f73f
sebastiangilpatino <sebastiangilpatino@gmail.com>
2021-04-25 08:38:56 -0500

    organizing output

--- homework.js
+++ homework.js
@@ -1,35 +1,34 @@
 let arr = [1, 50, 40, 3, 10];
 let result = arr.filter(x => x > 20)
   .reduce((accum, current) => accum + current);
-document.write('my array is ',arr,' sum of numbers greater than 20 is : ',result);
+document.write('My array is ',arr,' sum of numbers greater than 20 is : ',result);
 document.write("<br><br><br>");
-
 
 let strArr = ['maria', 'tomas', 'george', 'sebastian', 'mary'];
 let result2 = strArr.filter(x => x.length >= 5 && x.includes('a'));
-document.write('my array is '+strArr+' names length greater than 5 and including a is: ',result2);
+document.write('My array is '+strArr+' names length greater than 5 and including a is: ',result2);
 document.write("<br><br><br>");
 
-let birthDate1 = new Date(1994, 02, 14);
-let birthDate2 = new Date(1993, 04, 18);
-let birthDate3 = new Date(2011, 07, 11);
-let birthDate4 = new Date(1966, 02, 13);
-
-let emp1 = new Employee("jorge", "gil", birthDate1);
-let emp2 = new Employee("maria", "estrada", birthDate2);
-let emp3 = new Employee("tomas", "vargas", birthDate3);
-let emp4 = new Employee("leidy", "patino", birthDate4);
+let emp1 = new Employee("jorge", "gil", new Date(1994, 02, 14));
+let emp2 = new Employee("maria", "estrada", new Date(1993, 04, 18));
+let emp3 = new Employee("tomas", "vargas", new Date(2011, 07, 11));
+let emp4 = new Employee("leidy", "patino", new Date(1966, 02, 13));
 
 let emplArr = [emp1, emp2, emp3, emp4];
-document.write('Employees: ',emplArr);
-document.write("<br><br><br>");
+document.write('Employees are: ');
+document.write("<br>");
+printEmpl(emplArr);
+
+document.write("<br><br>");
 
 let greaterThan20 = emplArr.filter(x => x.getAge()>20);
-document.write('Employees age greater than 20 ',greaterThan20);
+document.write('Employees age greater than 20 years old');
+document.write("<br>");
+printEmpl(greaterThan20);
 document.write("<br><br><br>");
 
 let after2000 = emplArr.filter(x => x.getAge() > 21).map(x => x.getFullName());
-document.write('Employees born after 2000 ',after2000);
+document.write('Name of employees born after 2000: ',after2000);
 
 function Employee(firstName, lastName, birthDate) {
   this.firstName = firstName;
@@ -43,7 +42,14 @@ function Employee(firstName, lastName, birthDate) {
   this.getAge = function () {
     var difference= Math.abs(new Date() - this.birthDate);
     days = difference/(1000 * 3600 * 24)
-    return days/365;
+    return Math.round(days/365);
   }
 
+}
+
+function printEmpl(Empl){
+	for(let i=0; i<Empl.length; i++){
+		document.writeln('Employee '+i+': Name: '+Empl[i].getFullName()+', Age: '+Empl[i].getAge());
+		document.write("<br>");
+	}
 }
